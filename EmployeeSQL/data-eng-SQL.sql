9fc3baefd8a3360a3f2be0783c209c5451a8ceb0
Ryan McReynolds <ryan.a.mcreynolds@gmail.com>
2023-08-22 12:27:35 -0700

    cleaned/improved data eng code for table creation

--- EmployeeSQL/data-eng-SQL.sql
+++ EmployeeSQL/data-eng-SQL.sql
@@ -1,55 +1,47 @@
 
 CREATE TABLE titles (
-    title_id varchar(20) primary key,
-    title varchar(20)
+    title_id varchar(20) not null,
+    title varchar(20) not null,
+	primary key (title_id)
 );
 
 CREATE TABLE employees (
-    emp_no int primary key,
-    emp_title_id varchar(20),
-    birth_date varchar(20),
-    first_name varchar(20),
-    last_name varchar(20),
-    sex varchar(20),
-    hire_date varchar(20)
+    emp_no int not null,
+    emp_title_id varchar(20) not null,
+    birth_date varchar(20) not null,
+    first_name varchar(20) not null,
+    last_name varchar(20) not null,
+    sex varchar(20) not null,
+    hire_date varchar(20) not null,
+	primary key (emp_no),
+	foreign key (emp_title_id) references titles(title_id)
 );
 
 CREATE TABLE salaries (
-    emp_no int primary key,
-    salary int
+    emp_no int not null,
+    salary int not null,
+	primary key (emp_no),
+	foreign key (emp_no) references employees(emp_no)
 );
 
 CREATE TABLE departments (
-    dept_no varchar(20) primary key,
-    dept_name varchar(20)
+    dept_no varchar(20) not null,
+    dept_name varchar(20) not null,
+	primary key (dept_no)
 );
 
 CREATE TABLE dept_manager (
-    emp_no int primary key,
-    dept_no varchar(20)
+    emp_no int not null,
+    dept_no varchar(20) not null,
+	primary key (emp_no),
+	foreign key (emp_no) references employees(emp_no),
+	foreign key (dept_no) references departments(dept_no)
 );
 
 CREATE TABLE dept_emp (
-    emp_no int,
-    dept_no varchar(20),
-    primary key (emp_no, dept_no)
+    emp_no int not null,
+    dept_no varchar(20) not null,
+	primary key (emp_no, dept_no),
+	foreign key (emp_no) references employees(emp_no),
+	foreign key (dept_no) references departments(dept_no)
 );
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
